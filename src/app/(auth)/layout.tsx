b6045a91cd05a2c42833f5de96ae1c025b68eb3b
abhxnav <k.abhinav1000@gmail.com>
2024-09-26 12:57:55 +0530

    initial setup and routes

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="flex-center min-h-screen w-full bg-dark-400">Layout</main>
+  )
+}
+
+export default Layout
---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react'
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="flex min-h-screen w-full flex-col bg-dark-400 lg:flex-row">
+      <div className="mt-16 flex-1 overflow-auto py-8 lg:mt-0 lg:max-h-screen lg:py-10">
+        <div className="max-w-5xl mx-auto px-5 md:px-10 w-full text-dark-800 font-semibold text-base">
+          {children}
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Layout
