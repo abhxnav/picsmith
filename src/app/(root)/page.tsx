6fa253b797674a244d2b5b77dc926cd108237320
abhxnav <k.abhinav1000@gmail.com>
2024-10-02 21:00:26 +0530

    minor : build errors

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,17 +1,7 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
-
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-      {/* <div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </div> */}
     </div>
   )
 }
