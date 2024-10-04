1c7fc504044eb852e05522446cda83a19249bb87
abhxnav <k.abhinav1000@gmail.com>
2024-10-05 03:04:17 +0530

    feat [auth] : auth pages styling

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,22 @@
+import { Logo } from '@/components'
 import { ReactNode } from 'react'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="flex justify-center items-center min-h-screen w-full bg-dark-400 text-dark-800">
-      {children}
+    <main className="flex flex-col md:flex-row justify-center items-center min-h-screen w-full h-screen bg-dark-400 text-dark-800">
+      <div className="w-1/2 flex gap-2 md:gap-5 items-center justify-center pb-10 md:p-0">
+        <div>
+          <Logo variant="short" className="h-16 md:h-28 w-fit" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <Logo variant="text" className="h-10 md:h-20 w-fit" />
+          <p className="font-semibold text-[8px] md:text-sm">
+            CREATE MAGIC WITH A CLICK
+          </p>
+        </div>
+      </div>
+      <div className="w-px bg-dark-600 h-1/2 hidden md:block" />
+      <div className="w-1/2 flex items-center justify-center">{children}</div>
     </main>
   )
 }
--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -20,13 +20,19 @@ export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn
   if (!MONGODB_URL) throw new Error('MONGODB_URL is not defined')
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: 'picsmith',
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL, {
+      dbName: 'PicSmith',
       bufferCommands: false,
     })
+  }
 
-  cached.conn = await cached.promise
+  try {
+    cached.conn = await cached.promise
+  } catch (error) {
+    console.error('MongoDB connection failed:', error)
+    throw error
+  }
+
   return cached.conn
 }
