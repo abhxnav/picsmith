6c1c510b55c111e8695f1982bc284779bd40ea11
abhxnav <k.abhinav1000@gmail.com>
2024-10-15 20:36:18 +0530

    fix [logo] : logo redesign

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,19 +4,24 @@ import { ReactNode } from 'react'
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="flex flex-col md:flex-row justify-center items-center min-h-screen w-full h-screen bg-dark-400 text-dark-800">
-      <div className="w-1/2 flex gap-2 md:gap-5 items-center justify-center pb-10 md:p-0">
-        <div>
-          <Logo variant="short" className="h-16 md:h-28 w-fit" />
-        </div>
-        <div className="flex flex-col gap-1">
-          <Logo variant="text" className="h-10 md:h-20 w-fit" />
-          <p className="font-semibold text-[8px] md:text-sm">
-            CREATE MAGIC WITH A CLICK
-          </p>
-        </div>
+      <div className="w-full md:w-1/2 flex gap-2 md:gap-5 items-center justify-center pb-10 md:p-0">
+        <Logo variant="tag" className="h-20 md:h-40 w-fit md:px-8" />
+        {/* <div className="flex items-center gap-4">
+          <Logo variant="short" className="h-16 md:h-fit w-fit" />
+          <div className="flex flex-col">
+            <p className="font-dancing text-accent-400 text-2xl md:text-[110px] leading-none">
+              PicSmith
+            </p>
+            <p className="font-baloo text-dark-800 text-md md:text-3xl leading-none">
+              Create Magic With A Click
+            </p>
+          </div>
+        </div> */}
       </div>
       <div className="w-px bg-dark-600 h-1/2 hidden md:block" />
-      <div className="w-1/2 flex items-center justify-center">{children}</div>
+      <div className="w-full md:w-1/2 flex items-center justify-center">
+        {children}
+      </div>
     </main>
   )
 }
--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,17 +1,17 @@
 import Image from 'next/image'
 import FullLogo from '@/../../public/assets/logos/Logo_full.png'
 import ShortLogo from '@/../../public/assets/logos/Logo_short.png'
-import TextLogo from '@/../../public/assets/logos/Logo_text.png'
+import TagLogo from '@/../../public/assets/logos/Logo_tag.png'
 
 const Logo = ({
   variant = 'full',
   className,
 }: {
-  variant?: 'full' | 'short' | 'text'
+  variant?: 'full' | 'short' | 'tag'
   className?: string
 }) => {
   const logoType =
-    variant === 'full' ? FullLogo : variant === 'short' ? ShortLogo : TextLogo
+    variant === 'full' ? FullLogo : variant === 'short' ? ShortLogo : TagLogo
 
   return (
     <Image
